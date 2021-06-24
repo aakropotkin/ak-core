f7d51eed42dc44f25864c31d7b8d77d61532d514
aakropotkin <alex.ameen.tx@gmail.com>
2021-06-24 00:03:18 -0500

    Adding flake lock and install for 'scomm'

--- flake.nix
+++ flake.nix
@@ -1,11 +1,22 @@
 {
-  description = "A very basic flake";
+  description = "A handful of useful core utilities and scripts for Linux";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.ak-core =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "ak-core";
+        src  = self;
+        installPhase = ''
+          mkdir -p $out/bin;
+          install -t $out/bin src/zsh-scripts/scomm;
+        '';
+      };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.ak-core;
 
   };
 }
