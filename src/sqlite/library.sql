c3c287b6c0bb4034db0b0bbf684d3dbed5a6b9b3
aakropotkin <alex.ameen.tx@gmail.com>
2021-10-18 00:06:51 -0500

    add library.sql schema

---
+++ src/sqlite/library.sql
@@ -0,0 +1,84 @@
+CREATE TABLE SYMBOLS(
+    ID          INTEGER  PRIMARY KEY AUTOINCREMENT,
+    /* Usually `sym.<REALNAME>' */
+    FLAGNAME    TEXT     UNIQUE,
+    /* Demangled name */
+    NAME        TEXT     NOT NULL,
+    /* Mangled name */
+    REALNAME    TEXT,
+    /* Index in symtab */
+    ORDINAL     INTEGER  UNIQUE,
+    /* Global, Local, Weak, LoProc, HiProc */
+    BIND        TEXT,
+    /* NoType, Object, Func, Section, File, LoProc, HiProc */
+    TYPE        TEXT,
+    /* C, C++ */
+    LANG        TEXT,
+    /* 0 ( False ), 1 ( True ) */
+    IMPORTED    NUMERIC
+  );
+
+create table library(
+    ID           INTEGER PRIMARY KEY AUTOINCREMENT,
+    /* Filesystem Name */
+    FSNAME       TEXT,
+    SONAME       TEXT,
+    /* List of TEXT */
+    SYMLINKS     JSON,
+    REL_VERSION  TEXT,
+    ABI_VERSION  TEXT,
+    /* x86, aarch, ... */
+    ARCH         TEXT,
+    /* Base Address */
+    BADDR        INTEGER,
+    /* Size of the binary */
+    SIZE         INTEGER,
+    /* ELF, ??? */
+    BTYPE        TEXT,
+    /* 32, 64 */
+    BITS         INTEGER,
+    /* No idea */
+    CANARY       NUMERIC,
+    /* ELF32, ELF64 */
+    CLASS        TEXT,
+    /* GCC: (GNU) 10.3.0, ... */
+    COMPILER     TEXT,
+    /* ??? */
+    CRYPTO       NUMERIC,
+    /* 0 ( Little ), 1 ( BIG ) */
+    BIG_ENDIAN   NUMERIC,
+    /* 0 ( False ), 1 ( Has Debug sources ) */
+    HAVECODE     NUMERIC,
+    /* Logical Address */
+    LADDR        INTEGER,
+    /* C, C++, ... */
+    LANG         TEXT,
+    /* 0 ( False ), 1 ( Has Debug line numbers ) */
+    LINENUM      NUMERIC,
+    /* ?? */
+    LSYMS        NUMERIC,
+    /* AMD x86-64 architecture, ... */
+    MACHINE      TEXT,
+    /* ?? */
+    NX           NUMERIC,
+    /* linux, ... */
+    OS           TEXT,
+    /* 0 ( False ), 1 ( True ) */
+    PIC          NUMERIC,
+    /* 0 ( False ), 1 ( True ) */
+    RELOCS       NUMERIC,
+    /* "Relocations Read Only" ::= partial, ... */
+    RELRO        TEXT,
+    /* `:' separated list of paths */
+    RPATH        TEXT,
+    /* 0 ( False ), 1 ( True ) */
+    SANITIZED    NUMERIC,
+    /* 0 ( False ), 1 ( True ) */
+    STATIC       NUMERIC,
+    /* 0 ( False ), 1 ( True ) */
+    STRIPPED     NUMERIC,
+    /* linux, ... */
+    SUBSYSTEM    TEXT,
+    /* 0 ( False ), 1 ( True ) */
+    VA           NUMERIC
+  );
