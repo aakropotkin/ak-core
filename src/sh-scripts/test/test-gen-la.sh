9640bff1843021824eab08a421cb68aaa163422f
Alex Ameen <alex.ameen.tx@gmail.com>
2022-04-11 00:55:37 -0500

    test-gen-la.sh is POSIX

--- src/sh-scripts/test/test-gen-la.sh
+++ src/sh-scripts/test/test-gen-la.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
-
-export PATH=$( realpath -s $srcdir/..; ):$PATH;
+# scriptdir should always be set by the test driver
+: "${srcdir=$( dirname "$0"; )}";
+scriptdir="$( realpath -s "$srcdir/.."; )";
+export PATH="$scriptdir${PATH+:$PATH}";
 SCRIPT=gen-la;
 
 have_deps=yes;
@@ -13,25 +15,26 @@ test -f ./.libs/libgreet.so.0.0.0 || have_deps=no;
 test -f ./.libs/libgreet.a || have_deps=no;
 
 if test "x${have_deps}" != "xyes"; then
-  echo "${SCRIPT}: depends on ${PWD}/libgreet.la and ${PWD}/test/.libs/" >&2;
+  echo "$SCRIPT: depends on $PWD/libgreet.la and $PWD/test/.libs/"  \
+       > /dev/stderr;
   exit 1;
 fi
 
 
 TMP="$( mktemp; )";
-trap 'rm -f ${TMP}; exit 1;' HUP INT QUIT PIPE TERM;
+trap 'rm -f "$TMP"; exit 1;' HUP INT QUIT EXIT TERM;
 
-function try_recreate() {
-  gen-la ${1} > ${TMP};
-  rsl="${?}";
+try_recreate() {
+  gen-la "$1" > "$TMP";
+  rsl="$?";
 
-  if test "x${rsl}" != "x0"; then
-    echo "${SCRIPT}: encountered a runtime error" >&2;
-    exit ${rsl};
+  if test "$rsl" != "0"; then
+    echo "$SCRIPT: encountered a runtime error" > /dev/stderr;
+    exit $rsl;
   fi
 
-  if diff -s -I '^#' ${TMP} ./${2}|grep -q differ; then
-    echo "${SCRIPT}: failed to reproduce ${PWD}/${2}" >&2;
+  if diff -s -I '^#' "$TMP" "./$2"|grep -q differ; then
+    echo "$SCRIPT: failed to reproduce $PWD/$2" > /dev/stderr;
     exit 1;
   fi
 }
@@ -47,4 +50,4 @@ try_recreate ./.libs/libgreet.so.0.0.0 ./libgreet.la;
 try_recreate ./.libs/libgreet.a        ./libgreet.la;
 
 
-rm -f ${TMP};
+rm -f "$TMP";
