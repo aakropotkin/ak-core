c199a42f04a6557df138b44014c2e8b80ae4e4c8
Alex Ameen <alex.ameen.tx@gmail.com>
2022-04-15 20:19:04 -0500

    trying new style of flake organization for 2.7.0

--- flake.nix
+++ flake.nix
@@ -1,38 +1,29 @@
 {
   description = "A handful of useful core utilities and scripts for Linux";
 
-  inputs.flake-utils.url = github:numtide/flake-utils;
-
-  outputs = { self, nixpkgs, ... }: {
-    overlays.ak-core = import ./overlay.nix;
-
-    packages.x86_64-linux.ak-core = ( import nixpkgs {
-      sys = "x86_64-linux";
-      overlays = [self.overlays.ak-core];
-    } ).ak-core;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.ak-core;
-
-    packages.aarch64-linux.ak-core = ( import nixpkgs {
-      sys = "aarch64-linux";
-      overlays = [self.overlays.ak-core];
-    } ).ak-core;
-    packages.aarch64-linux.default = self.packages.aarch64-linux.ak-core;
-
-    packages.x86_64-darwin.ak-core = ( import nixpkgs {
-      sys = "x86_64-darwin";
-      overlays = [self.overlays.ak-core];
-    } ).ak-core;
-    packages.x86_64-darwin.default = self.packages.x86_64-darwin.ak-core;
-
-    packages.aarch64-darwin.ak-core = ( import nixpkgs {
-      sys = "aarch64-darwin";
-      overlays = [self.overlays.ak-core];
-    } ).ak-core;
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.ak-core;
-
-    nixosModules.ak-core = { pkgs, ... }: {
-      nixpkgs.overlays = [self.overlays.ak-core];
-    };
-    nixosModule = self.nixosModules.ak-core;
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    utils.url   = github:numtide/flake-utils;
   };
+
+  outputs = { self, nixpkgs, utils }:
+    let
+      eachDefaultSystemMap = utils.lib.eachSystemMap utils.lib.defaultSystems;
+    in {
+      packages = eachDefaultSystemMap ( system: rec {
+        ak-core =
+          ( import nixpkgs { inherit system;} ).callPackage ./default.nix {};
+        default = ak-core;
+      } );
+
+      overlays.ak-core = final: prev: {
+        inherit (self.packages.${final.system}) ak-core;
+      };
+      overlays.default = self.overlays.ak-core;
+
+      nixosModules.ak-core = { ... }: {
+        nixpkgs.overlays = builtins.attrValues self.overlays;
+      };
+      nixosModule = self.nixosModules.ak-core;
+    };
 }
